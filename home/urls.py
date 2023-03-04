cbb7e822411c37fa1899f7f5aa65cd18b9b9792b
bivek <karki777bivek@gmail.com>
2023-03-04 21:19:27 +0545

    manage path for static files

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path
 
 from home import views
+app_name="home"
 
 urlpatterns = [
-    path('', views.index, name='home-page'),
+    path('', views.home, name='home-page'),
 ]
